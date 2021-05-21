50a8032d6edd43e896844133f084ae69a861c2e1
dinggu <zaqxsw9705@naver.com>
2021-05-22 03:39:36 +0900

    [💄] <payment.vue파일에서 결제페이지의 배송지 파트까지 코드를 작성했습니다.>
    [📝]<readme 파일에서 저와 성우님이 맡은 페이지가 무엇인지 작성 하였습니다.>

--- README.md
+++ README.md
@@ -22,8 +22,8 @@ npm run serve
 ### 페이지 분담 🎈
 
 Shipping(배송) => </br>
-Cart(장바구니), Payment(결제) => 동호</br>
-Product(제품상세) => </br>
+Cart(장바구니), Payment(결제) => 동호 </br>
+Product(제품상세) Home(홈)=> 성우 </br>
 Login(로그인), SignUp(회원가입), UserProfileUpdate(회원정보수정), Footer(푸터), NavBar(네비게이션바)  => 상혁 </br>
 </br></br>
 
--- src/views/Payment.vue
+++ src/views/Payment.vue
@@ -1,10 +1,126 @@
 <template>
-  <div id="payment">
-    <!-- 이곳을 자유롭게 만들어주세요. -->
-    <p>결제 페이지</p>
-    <p>test</p>
-    <p>test</p>
-    <!-- 결제 페이지 입니다. -->
+  <div class="container" style="padding-right:400px; padding-left:400px;">
+    <div class="row border-bottom border-5">
+      <div class="fw-bolder fs-4 text-dark pb-4">신선한 못난이</div>
+    </div>
+    <div class="row">
+      <div class="fw-bold fs-5 text-dark pt-4 pb-4">주문 상품</div>      
+    </div>
+    <div class="row pb-4">
+        <div class="col-md-3">
+          <a class="align-self-center" style="cursor:pointer;"><img src="https://contents.sixshop.com/uploadedFiles/48764/product/image_1620794006376.jpg" style="width: 90px; height: 90px;"></a>
+        </div>
+        <div class="col-md-9">
+          <div class="text-dark pb-3">국산 햇양파 10kg</div>
+          <div>
+            <span class="text-dark">1개</span>
+            <span class="text-dark"> / </span>
+            <span class="text-dark">21,900원</span>        
+          </div>
+        </div>
+    </div>
+    <div class="row pb-4">
+        <div class="col-md-3">
+          <a class="align-self-center" style="cursor:pointer;"><img src="https://contents.sixshop.com/uploadedFiles/48764/product/image_1620794006376.jpg" style="width: 90px; height: 90px;"></a>
+        </div>
+        <div class="col-md-9">
+          <div class="text-dark pb-3">국산 햇양파 10kg</div>
+          <div>
+            <span class="text-dark">1개</span>
+            <span class="text-dark"> / </span>
+            <span class="text-dark">21,900원</span>        
+          </div>
+        </div>
+    </div>
+    <div class="row border-top pb-4 pt-4">
+        <span class="col-md-6 text-dark align-self-center fw-bolder">상품 합계</span>
+        <span class="col-md-6 text-end fs-5 fw-bolder text-primary align-self-center">48,400원</span>  
+    </div>
+    <div class="row border-top border-5">
+        <div class="text-dark fw-bold fs-5 pt-4 pb-4">주문자</div>
+        <div class="text-dark pb-1">최동호</div>
+        <div class="text-dark pb-1">010-1234-5678</div>
+        <div class="text-dark pb-1">zaqxsw9705@naver.com</div>     
+    </div>
+    <div class="row pt-4 pb-4">
+        <button type="button" class="btn btn-outline-primary">수정하기</button>
+    </div>
+    <div class="row border-top border-5">
+        <div class="text-dark fw-bold fs-5 pt-4 pb-5">배송지</div>
+    </div>
+    <div class="row" style="padding-right:10px; padding-left:10px;">
+        <div class="form-check col-md-3">
+          <label class="form-check-label">
+            <input type="radio" class="form-check-input" name="optionsRadios" id="optionsRadios1" value="option1" checked="">
+            기본배송지
+          </label>
+        </div>
+        <div class="form-check col-md-3">
+          <label class="form-check-label">
+            <input type="radio" class="form-check-input" name="optionsRadios" id="optionsRadios1" value="option1" checked="">
+            신규배송지
+          </label>
+        </div>
+        <div class="col-md-6 align-self-center pb-3">
+          <button type="button" class="btn btn-outline-primary btn-sm">배송지 목록</button>
+        </div>
+    </div>     
+    <div class="form-group pb-1">
+        <label for="exampleInputEmail1" class="form-label mt-4 text-dark">이름</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    </div>        
+    <label for="exampleInputEmail1" class="form-label mt-4 text-dark">연락처</label>
+    <div class="row" style="padding-right: 10px; padding-left: 10px;">
+        <input type="email" class="form-control" style="width: 30%;" id="exampleInputEmail1" aria-describedby="emailHelp">
+        <div class="align-self-center" style="width:5%;">-</div>
+        <input type="email" class="form-control" style="width: 30%;" id="exampleInputEmail1" aria-describedby="emailHelp">
+        <div class="align-self-center" style="width:5%;">-</div>
+        <input type="email" class="form-control" style="width: 30%;" id="exampleInputEmail1" aria-describedby="emailHelp">
+    </div>        
+    <div class="form-group">
+        <label for="exampleInputEmail1" class="form-label mt-4 text-dark">배송지명</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    </div>
+    <label for="exampleInputEmail1" class="form-label mt-4 text-dark">우편번호</label>
+    <div class="row form-group">
+      <div class="col-md-9">
+        <input type="text" class="form-control" id="inputDefault">      
+      </div>   
+      <div class="col-md-3 d-flex justify-content-end">
+        <button type="button" class="btn btn-outline-primary">검색하기</button>
+      </div>
+    </div>
+    <div class="row">
+        <div class="form-group">
+          <label class="col-form-label mt-3 text-dark" for="inputDefault">주소</label>
+          <div class="pb-2">
+              <input type="text" class="form-control" id="inputDefault"> 
+          </div>
+          <div>
+              <input type="text" class="form-control" id="inputDefault">
+          </div>
+        </div>
+    </div>
+    <div class="row pt-3 pb-3 border-bottom" style="padding-left:12px;">
+        <div class="form-check col-md-6">
+          <input class="form-check-input" type="checkbox" id="flexCheckDefault">
+          <label class="form-check-label" for="flexCheckDefault">
+            기본 배송지로 설정하기
+          </label>
+        </div>
+        <div class="form-check col-md-6">
+          <input class="form-check-input" type="checkbox" id="flexCheckDefault">
+          <label class="form-check-label" for="flexCheckDefault">
+            배송지목록에 추가하기
+          </label>
+        </div>      
+    </div>
+    <div class="row pb-5 pt-2">
+      <div class="form-group">       
+        <label class="col-form-label text-dark" for="inputDefault">배송 시 요청 사항</label>       
+        <input type="text" class="form-control" id="inputDefault">
+      </div>
+    </div>       
   </div>
 </template>
 
