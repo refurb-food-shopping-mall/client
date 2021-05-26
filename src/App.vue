2cd8e520e3d309f234c675cee9f06131af145619
Jason <1127lse@gmail.com>
2021-05-26 14:31:07 +0900

    💄 header 는 sticky하게 footer는 하단에 존재하도록 app div에 flex 적용

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,37 @@
 <template>
   <div id="app">
     <nav-bar />
-    <div class="container my-3">
+    <div class="inner_app container my-3">
       <router-view />
     </div>
-    <!-- <Footer /> -->
+    <Footer />
   </div>
 </template>
 
 <script>
 import NavBar from "./components/NavBar";
-// import Footer from "./components/Footer";
+import Footer from "./components/Footer";
 
 export default {
-  components: { NavBar },
+  components: { NavBar, Footer },
 };
 </script>
 
 <style>
+html,
+body {
+  height: 100%;
+}
+.inner_app {
+  flex: 1 0 auto;
+}
+#navbar {
+  position: sticky;
+  top: 0;
+}
 #app {
-  padding-top: 70px;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
 }
 </style>
