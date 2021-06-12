4822b5eb0c81df2dc319095d42de0faf2335389a
DESKTOP-P1UNOTJ\jason <1127lse@gmail.com>
2021-06-12 15:40:23 +0900

    🔧 proxy 설정 임시 보류 - 추후에 사용예정. 💄 main.js axios mixin function 사용 보류 & prototype 등록으로 사용 - mixin은 추후에 사용예정 💄 Login.vue 서버 응답에 따른 로그인 로직 처리 🍱 loading bar로 사용할 svg 추가

--- src/main.js
+++ src/main.js
@@ -13,21 +13,25 @@ library.add(faUserSecret, faShoppingCart, faUser, faSearch)
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
+Vue.prototype.$axios = axios
+Vue.prototype.$domain = "http://localhost:3000/api"
+
 Vue.config.productionTip = false
 
-Vue.mixin({
-  methods: {
-    async $api(url, data){
-      return (await axios({
-          method: 'post',
-          url,
-          data
-      }).catch(e => {
-          console.log(e)
-      })).data;
-    }
-  }
-})
+// Vue.mixin({
+//   methods: {
+//     async $api(url, data){
+//       return (await axios({
+//           method: 'post',
+//           url,
+//           data
+//       }).catch(e => {
+//           console.log(e)
+//       })).data;
+//     }
+//   }
+// })
+
 
 new Vue({
   router,
