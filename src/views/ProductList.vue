087f1aefdeaf3a1577bb3a25bd20b4175c719420
studentinbb <studentinbb@gmail.com>
2021-07-03 13:58:55 +0900

    홈 페이지 수정, 제품 목록 페이지 추가, 네비게이션 바 수정

---
+++ src/views/ProductList.vue
@@ -0,0 +1,72 @@
+<template>
+  <div id="productlist">
+    <!-- Page Content-->
+    <div class="container px-4 px-lg-5">      
+      <!-- Content Row-->
+      <div class="row gx-4 gx-lg-5">
+        <!-- 카드 1 -->
+        <div
+          v-for="(product, idx) in productList"
+          :key="idx"
+          class="col-md-4 mb-5"
+        >
+          <div class="card h-100">
+            <img :src="getImgUrl(product)" class="img-fluid" />
+            <div class="card-body">
+              <div class="my-3">
+                <h5 class="card-title" style="text-align: center">
+                  {{ product.product_name }}
+                </h5>
+              </div>
+              <p class="card-text" style="text-align: center">
+                {{product.harvested_date}} 수확 상품
+              </p>
+            </div>
+            <div
+              class="
+                card-footer
+                d-flex
+                justify-content-between
+                align-items-center
+              "
+            >
+              <div>
+                <b>{{ product.product_price.toLocaleString("ko-KR") }}원</b>
+              </div>
+              <router-link
+                :to="`/product/${product.id}`"
+                class="btn btn-primary btn-sm"
+                >사러 가기</router-link
+              >
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      productList: [],
+    };
+  },
+  mounted() {
+    this.$axios.get(`${this.$domain}/product`).then((res) => {
+      this.productList = res.data;
+      console.log(this.productList);
+    });
+  },
+  methods: {
+    getImgUrl(product) {
+      let pic = product.t_product_images[0].path.split("/")[2];
+      return require("../assets/" + pic);
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
