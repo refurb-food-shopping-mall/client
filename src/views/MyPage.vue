33f36b1fca24fcb6c9015ca22dca25b836425389
DESKTOP-P1UNOTJ\jason <1127lse@gmail.com>
2021-07-07 13:17:03 +0900

    💄 [ MyPage.vue ] myPage menublock 데이터 설정 & 바인딩

--- src/views/MyPage.vue
+++ src/views/MyPage.vue
@@ -4,33 +4,21 @@
       <!-- Left Menu List -->
       <div class="test-b col-lg-3 d-flex justify-content-center">
         <div class="list-group col-9 text-center">
-          <router-link
-            to="#"
-            class="list-group-item list-group-item-action active"
-            >주문 조회
-          </router-link>
-          <router-link to="/cart" class="list-group-item list-group-item-action"
-            >장바구니
-          </router-link>
-          <router-link
-            to="/cancel"
-            class="list-group-item list-group-item-action"
-            >취소 조회
-          </router-link>
-          <router-link to="#" class="list-group-item list-group-item-action"
-            >적립금
-          </router-link>
-          <router-link to="#" class="list-group-item list-group-item-action"
-            >1:1 문의
-          </router-link>
-          <router-link
-            to="/mypage/profileupdate"
-            class="list-group-item list-group-item-action"
-            >회원정보수정
-          </router-link>
-          <router-link to="#" class="list-group-item list-group-item-action"
-            >회원탈퇴
-          </router-link>
+          <div>
+            <div
+              class="menu__block"
+              v-for="(menuLink, idx) in menuLinks"
+              :key="idx"
+              @click.prevent="currentSelectdMenuIdx = idx"
+            >
+              <router-link
+                :to="menuLink.url"
+                class="list-group-item list-group-item-action"
+                :class="{ active: idx == currentSelectdMenuIdx }"
+                >{{ menuLink.name }}
+              </router-link>
+            </div>
+          </div>
         </div>
       </div>
       <!-- Content View -->
@@ -44,12 +32,54 @@
 
 <script>
 export default {
-
+  data() {
+    return {
+      currentSelectdMenuIdx: 0,
+      menuLinks: [
+        {
+          url: '#',
+          name: '주문 조회'
+        },
+        {
+          url: '/cart',
+          name: '장바구니'
+        },
+        {
+          url: '/cancel',
+          name: '취소 조회'
+        },
+        {
+          url: '#',
+          name: '적립금'
+        },
+        {
+          url: '#',
+          name: '1:1 문의'
+        },
+        {
+          url: '/mypage/profileupdate',
+          name: '회원정보수정'
+        },
+        {
+          url: '#',
+          name: '회원탈퇴'
+        }
+      ]
+    }
+  },
 }
 </script>
 
 <style scoped>
 .test-b {
   /* border: 2px dashed green; */
+}
+.menu__block:first-child {
+  border-top-right-radius: 6.4px;
+  border-top-left-radius: 6.4px;
+}
+.menu__block:last-child {
+  border-bottom-right-radius: 6.4px;
+  border-bottom-left-radius: 6.4px;
 }
 </style>
