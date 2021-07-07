234e02ae4a6d9d71ec70d6c329b2740708bbe342
DESKTOP-P1UNOTJ\jason <1127lse@gmail.com>
2021-07-07 13:24:00 +0900

    💄 [ MyPageMenuDrawer.vue ]  MyPageMenuDrawer 컴포넌트 생성 💄 [ MyPage.vue ] MyPageMenuDrawer 컴포넌트 등록 & 사용

--- src/views/MyPage.vue
+++ src/views/MyPage.vue
@@ -4,26 +4,11 @@
       <!-- Left Menu List -->
       <div class="test-b col-lg-3 d-flex justify-content-center">
         <div class="list-group col-9 text-center">
-          <div>
-            <div
-              class="menu__block"
-              v-for="(menuLink, idx) in menuLinks"
-              :key="idx"
-              @click.prevent="currentSelectdMenuIdx = idx"
-            >
-              <router-link
-                :to="menuLink.url"
-                class="list-group-item list-group-item-action"
-                :class="{ active: idx == currentSelectdMenuIdx }"
-                >{{ menuLink.name }}
-              </router-link>
-            </div>
-          </div>
+          <MyPageMenuDrawer></MyPageMenuDrawer>
         </div>
       </div>
       <!-- Content View -->
       <div class="test-b col-lg-9 d-flex justify-content-center">
-        <!-- Content View -->
         <router-view></router-view>
       </div>
     </div>
@@ -31,40 +16,12 @@
 </template>
 
 <script>
+import MyPageMenuDrawer from '@/components/MyPageMenuDrawer'
+
 export default {
+  components: { MyPageMenuDrawer },
   data() {
     return {
-      currentSelectdMenuIdx: 0,
-      menuLinks: [
-        {
-          url: '#',
-          name: '주문 조회'
-        },
-        {
-          url: '/cart',
-          name: '장바구니'
-        },
-        {
-          url: '/cancel',
-          name: '취소 조회'
-        },
-        {
-          url: '#',
-          name: '적립금'
-        },
-        {
-          url: '#',
-          name: '1:1 문의'
-        },
-        {
-          url: '/mypage/profileupdate',
-          name: '회원정보수정'
-        },
-        {
-          url: '#',
-          name: '회원탈퇴'
-        }
-      ]
     }
   },
 }
@@ -73,13 +30,5 @@ export default {
 <style scoped>
 .test-b {
   /* border: 2px dashed green; */
-}
-.menu__block:first-child {
-  border-top-right-radius: 6.4px;
-  border-top-left-radius: 6.4px;
-}
-.menu__block:last-child {
-  border-bottom-right-radius: 6.4px;
-  border-bottom-left-radius: 6.4px;
 }
 </style>
