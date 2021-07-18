2da40500a530754a868af73f4e978a090e65cfbe
DESKTOP-P1UNOTJ\jason <1127lse@gmail.com>
2021-07-18 18:07:45 +0900

    💄 home - banner1 component 생성 및 기존 레이아웃 container 부분 조정

---
+++ src/components/HomeLayout/banner-1.vue
@@ -0,0 +1,44 @@
+<template>
+  <section class="banner banner-1">
+    <div class="container">
+      <div class="row justify-content-end text-black">
+        <div class="col-md-6 ps-4">
+          <div class="fs-5 title">
+            <span> <strong>여름 시즌 고객 감사제</strong> </span>
+          </div>
+          <div class="my-2 fs-2">
+            <span class="title">
+              <strong>
+                채소는 오늘도 착함 <br />
+                못난이 채소 모음전
+              </strong>
+            </span>
+          </div>
+          <div class="my-2">
+            <span>EVENT 21.07.24 ~ 21.08.07 </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.banner {
+  padding: 1.5em 0;
+  background-image: url("../../assets/vegetable_3.jpeg");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center center;
+  cursor: pointer;
+}
+.title {
+  color: black;
+}
+</style>
