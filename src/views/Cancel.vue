2e2da848fdaa0c7c8aceeb7370f2b4018db8d5c0
studentinbb <studentinbb@gmail.com>
2021-07-24 11:00:46 +0900

    교환/환불 요청 기능 추가

--- src/views/Cancel.vue
+++ src/views/Cancel.vue
@@ -89,7 +89,7 @@
                 </div>
               </div>
           </div>
-          <div v-if="togg" class="row border-bottom border-2 pt-3 pb-3">
+          <div v-if="togg===1" class="row border-bottom border-2 pt-3 pb-3">
             <div class="col-md-2">교환 사유</div>
             <div class="col">
               <div class="form-group">
@@ -100,7 +100,7 @@
               </div>
             </div>
           </div>
-          <div v-else class="row border-bottom border-2 pt-3 pb-3">
+          <div v-else-if="togg===0" class="row border-bottom border-2 pt-3 pb-3">
             <div class="col-md-2">환불 사유</div>
             <div class="col">
               <div class="form-group">
@@ -130,10 +130,10 @@
 
         <div class="d-flex justify-content-end">
           <router-link :to="`/shipping`" class="btn btn-primary m-2">이전 페이지</router-link>
-          <button v-if="togg" type="button" class="btn btn-secondary my-2">
+          <button @click="exchange()" v-if="togg===1" type="button" class="btn btn-secondary my-2">
             교환 요청하기
           </button>
-          <button v-else type="button" class="btn btn-secondary my-2">
+          <button @click="refund()" v-else-if="togg===0" type="button" class="btn btn-secondary my-2">
             환불 요청하기
           </button>
         </div>
@@ -145,10 +145,11 @@
 </template>
 
 <script>
+import Swal from 'sweetalert2';
 export default {
   data() {
     return {
-      togg : 1,
+      togg : 2,
       image: {},
       product: {
         product_name : "",
@@ -163,6 +164,7 @@ export default {
   },
   mounted() {
     this.getOrder();
+    console.log(this.$router.currentRoute)
   },
   methods: {
      getOrder() {
@@ -182,7 +184,80 @@ export default {
     },
     toggle(number) {
       this.togg = number;
-    }
+    },
+    // exchange() {
+    //   this.$axios
+    //   .post(`${this.$domain}/exchange`, {
+    //     id: this.$route.params.id
+    //   })
+    //   .then(res => {
+    //     console.log(res.data)
+    //     alert('교환 요청되었습니다')
+    //   })
+    //   this.$router.push("/shipping")
+    // },
+    exchange() {
+      Swal.fire({
+      title: '교환 요청 하시겠습니까?',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#5ab395',
+      cancelButtonColor: '#F08080',
+      confirmButtonText: '예',
+      cancelButtonText: "아니오",
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        await this.$axios
+          .post(`${this.$domain}/exchange`, {
+            id : this.$route.params.id,
+          })
+          .then((res) => {
+            console.log(res.data);
+          });
+        this.exchange2 ();
+      }
+    })
+    },
+    async exchange2 (){
+      await Swal.fire({
+      title: '교환 요청되었습니다!',
+      icon: 'success',
+      confirmButtonText: '확인',
+      confirmButtonColor: '#5ab395',
+    })
+    this.$router.push("/shipping");
+    },
+    refund() {
+      Swal.fire({
+      title: '환불 요청 하시겠습니까?',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#5ab395',
+      cancelButtonColor: '#F08080',
+      confirmButtonText: '예',
+      cancelButtonText: "아니오",
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        await this.$axios
+          .post(`${this.$domain}/exchange`, {
+            id : this.$route.params.id,
+          })
+          .then((res) => {
+            console.log(res.data);
+          });
+        this.refund2 ();
+      }
+    })
+    },
+    async refund2 (){
+      await Swal.fire({
+      title: '환불 요청되었습니다!',
+      icon: 'success',
+      confirmButtonText: '확인',
+      confirmButtonColor: '#5ab395',
+    })
+    this.$router.push("/shipping");
+    },
   }
 };
 </script>
